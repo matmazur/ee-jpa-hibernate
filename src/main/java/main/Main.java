2b109945cf534ec456984e3dab1b372955dffa16
Bingo Bobo <matmazur90@gmail.com>
2018-09-21 15:32:05 +0200

    testing connection in Main class

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,11 +1,28 @@
 package main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main
 {
     public static void main(String[] args) {
 
 
 
+        String jdbcUrl = "jdbc:mysql://localhost:3306/test?useSSL=false";
+        String user = "root";
+        String password = "pass";
+
+        System.out.println("Connecting to the DB + " + jdbcUrl);
+
+        try {
+            Connection myConn = DriverManager.getConnection(jdbcUrl, user, password);
+            System.out.println("Connection successful");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+// THIS ABOVE WORKS CORRECLTY  -  CONNECTION TO THE DB IS PROPER
 
 
     }
