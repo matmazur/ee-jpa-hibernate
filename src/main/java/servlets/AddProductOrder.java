818ecda66e117122fba9611d7afc4f9020dd3ac9
Bingo Bobo <matmazur90@gmail.com>
2018-09-26 14:04:48 +0200

    filled AddProductOrder with actual servlet adding to the order products

--- src/main/java/servlets/AddProductOrder.java
+++ src/main/java/servlets/AddProductOrder.java
@@ -1,4 +1,42 @@
 package servlets;
 
-public class AddProductOrder {
+import dao.OrderDAO;
+import dao.ProductDAO;
+import model.Order;
+import model.Product;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/add-product-order")
+public class AddProductOrder extends HttpServlet {
+
+    @Inject
+    OrderDAO orderDAO;
+    @Inject
+    ProductDAO productDAO;
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+        Long orderId = Long.valueOf(req.getParameter("orderId"));
+        Long productId = Long.valueOf(req.getParameter("productId"));
+
+        Order order = orderDAO.read(orderId);
+        Product product = productDAO.read(productId);
+
+
+        order.getProducts().add(product);
+
+        resp.sendRedirect(getServletContext().getContextPath());
+
+
+    }
 }
