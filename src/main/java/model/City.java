edd2c031abaef57f31119560ffeb550bb9c49349
Bingo Bobo <matmazur90@gmail.com>
2018-09-27 14:49:18 +0200

    added model entity City

---
+++ src/main/java/model/City.java
@@ -0,0 +1,80 @@
+package model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "city")
+public class City {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "Name")
+    private String name;
+
+    @Column(name = "CountryCode")
+    private String countryCode;
+
+    @Column(name = "District")
+    private String district;
+
+    @Column(name = "Population")
+    private Long population;
+
+    public City() {
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", countryCode='" + countryCode + '\'' +
+                ", district='" + district + '\'' +
+                ", population=" + population +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public Long getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Long population) {
+        this.population = population;
+    }
+}
