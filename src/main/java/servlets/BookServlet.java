4f652b69244037bbe412065b5db08102eb32f226
Bingo Bobo <matmazur90@gmail.com>
2018-09-28 14:15:17 +0200

    small refactoring in BookServlet

--- src/main/java/servlets/BookServlet.java
+++ src/main/java/servlets/BookServlet.java
@@ -20,38 +20,40 @@ public class BookServlet extends HttpServlet {
     @Inject
     BookDao dao;
 
-
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         String option = req.getParameter("form");
-        if (option.equals("add")){
-            String isbn = req.getParameter("isbn");
-            String author = req.getParameter("author");
-            String title = req.getParameter("title");
-
-            Book book = new Book();
-            book.setAuthor(author);
-            book.setIsbn(isbn);
-            book.setTitle(title);
-
-            dao.save(book);
+        if (option.equals("add")) {
+            saveBook(req);
         }
-        if (option.equals("jpql")){
-
-            String query = req.getParameter("query");
-            List<Book> books = dao.doQuery(query);
-
-            for(Book b:books) {
-                resp.getWriter().println(b);
-            }
-            req.setAttribute("query", query);
-            req.setAttribute("books", books);
-
+        if (option.equals("jpql")) {
+            commitQuery(req, resp);
             RequestDispatcher dispatcher = req.getRequestDispatcher("result.jsp");
-            dispatcher.forward(req,resp);
+            dispatcher.forward(req, resp);
         }
 
 
+    }
+
+    private void commitQuery(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String query = req.getParameter("query");
+        List<Book> books = dao.doQuery(query);
+
+        req.setAttribute("query", query);
+        req.setAttribute("books", books);
+    }
+
+    private void saveBook(HttpServletRequest req) {
+        String isbn = req.getParameter("isbn");
+        String author = req.getParameter("author");
+        String title = req.getParameter("title");
+
+        Book book = new Book();
+        book.setAuthor(author);
+        book.setIsbn(isbn);
+        book.setTitle(title);
+
+        dao.save(book);
     }
 }
